f36bda4592a5af47c8beb36f8c26aee4b1a06d1d
llattanzi <lauren71442@gmail.com>
2022-03-12 20:13:37 -0500

    add show/hide function to done and highscore, set initial visibility in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,16 @@
 
 var quizLength = $(".question").length;
 
-// initial quiz set-up function, hides everything but the welcome screen
-var startQuiz = function() {
-    $("#quiz-welcome").show();
-    $(".question").hide();
-    $("#result").hide();
-    $("#done").hide();
-    $("#high-scores").hide();
-}
-
 // start quiz button was clicked
 $("#start-quiz").click(function() {
     $("#quiz-welcome").hide();
     var questionStart = 1;
-    askQuestion(questionStart)
-    
+    askQuestion(questionStart);
 })
 
 var askQuestion = function(questionNum) {
     $("#question-" + questionNum).show();
-
+    console.log($(this))
 
     $(".answer").click(function() { 
         $("#question-" + questionNum).hide();
@@ -35,6 +25,26 @@ var askQuestion = function(questionNum) {
     });
 }
 
+// on submit button click save score to local storage and show high score screen
+$("#save-score").click(function(){
+    $("#main-content").hide();
+    $("#header").css("visibility", "hidden");
+    $("#high-scores").show();
+})
 
+// "View high scores" in header is clicked
+// user can only switch to highscore screen when not on quiz question screens
+$("#view-score").on("click", function() {
+    if (!$(".question").is(":visible")) {
+        $("#header").css("visibility", "hidden");
+        $("#main-content").hide();
+        $("#high-scores").show();
+    }
+})
 
-startQuiz();
+// show previous screen when go back is clicked
+$("#go-back").click(function() {
+    $("#high-scores").hide();
+    $("#main-content").show();
+    $("#header").css("visibility", "visible");
+})
